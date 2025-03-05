18e5c0ac5672fb844f5cd0928be108d42a61e868
zonghai <zonghai_zeng@163.com>
2025-03-05 09:40:35 +0100

    默认发音，点击单词跳转

--- src/components/WordDefinition.tsx
+++ src/components/WordDefinition.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Play } from 'lucide-react';
+import usePronunciationSound from '../hooks/usePronunciation'
 import { openYouglish } from './commonFunctions';
 
 interface WordDefinitionProps {
@@ -15,13 +16,29 @@ const WordDefinition: React.FC<WordDefinitionProps> = ({
   exampleSentences,
 }) => {
 
+  const { play, stop, isPlaying } = usePronunciationSound(word)
+
+  const playSound = useCallback(() => {
+    stop()
+    play()
+  }, [play, stop])
+
+  useEffect(() => {
+    return stop
+  }, [word, stop])
+
   return (
     <div className="bg-white rounded-lg shadow-md p-6 mt-4 relative">
       <h2 className="text-6xl font-bold mb-2">
-        {word}{' '}
+      <span
+        className="cursor-pointer text-blue-500"
+        onClick={() => openYouglish(word)}
+        >
+          {word} {' '} 
+        </span>
         <button
           className="text-blue-500 hover:text-blue-700 focus:outline-none"
-          onClick={() => openYouglish(word)}
+          onClick={playSound}
         >
           <Play className="inline-block h-10 w-10" />
         </button>
---
+++ src/hooks/usePronunciation.ts
@@ -0,0 +1,119 @@
+import type { Howl } from 'howler'
+import { useEffect, useMemo, useState } from 'react'
+import useSound from 'use-sound'
+
+export type PronunciationType = 'us' | 'uk' | 'zh' 
+export const pronunciationConfigAtom = {
+  isOpen: true,
+  volume: 1,
+  type: 'us' as PronunciationType,
+  name: 'English',
+  isLoop: false,
+  isTransRead: false,
+  transVolume: 1,
+  rate: 1,
+}
+
+export declare type SpriteMap = {
+  [key: string]: [number, number];
+};
+
+export declare type HookOptions<T = any> = T & {
+  id?: string;
+  volume?: number;
+  playbackRate?: number;
+  interrupt?: boolean;
+  soundEnabled?: boolean;
+  sprite?: SpriteMap;
+  onload?: () => void;
+};
+
+export function addHowlListener(howl: Howl, ...args: Parameters<Howl['on']>) {
+  howl.on(...args)
+
+  return () => howl.off(...args)
+}
+
+export function noop(): void {}
+
+const pronunciationApi = 'https://dict.youdao.com/dictvoice?audio='
+export function generateWordSoundSrc(word: string, pronunciation: Exclude<PronunciationType, false>): string {
+  switch (pronunciation) {
+    case 'uk':
+      return `${pronunciationApi}${word}&type=1`
+    case 'us':
+      return `${pronunciationApi}${word}&type=2`
+    case 'zh':
+      return `${pronunciationApi}${word}&le=zh`
+    default:
+      return ''
+  }
+}
+
+export default function usePronunciationSound(word: string, isLoop?: boolean) {
+  const pronunciationConfig = pronunciationConfigAtom
+  const loop = useMemo(() => (typeof isLoop === 'boolean' ? isLoop : pronunciationConfig.isLoop), [isLoop, pronunciationConfig.isLoop])
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const [play, { stop, sound }] = useSound(generateWordSoundSrc(word, pronunciationConfig.type), {
+    html5: true,
+    format: ['mp3'],
+    loop,
+    volume: pronunciationConfig.volume,
+    rate: pronunciationConfig.rate,
+  } as HookOptions)
+
+  useEffect(() => {
+    if (!sound) return
+    sound.loop(loop)
+    return noop
+  }, [loop, sound])
+
+  useEffect(() => {
+    if (!sound) return
+    const unListens: Array<() => void> = []
+
+    unListens.push(addHowlListener(sound, 'play', () => setIsPlaying(true)))
+    unListens.push(addHowlListener(sound, 'end', () => setIsPlaying(false)))
+    unListens.push(addHowlListener(sound, 'pause', () => setIsPlaying(false)))
+    unListens.push(addHowlListener(sound, 'playerror', () => setIsPlaying(false)))
+
+    return () => {
+      setIsPlaying(false)
+      unListens.forEach((unListen) => unListen())
+      ;(sound as Howl).unload()
+    }
+  }, [sound])
+
+  return { play, stop, isPlaying }
+}
+
+export function usePrefetchPronunciationSound(word: string | undefined) {
+  const pronunciationConfig = pronunciationConfigAtom
+
+  useEffect(() => {
+    if (!word) return
+
+    const soundUrl = generateWordSoundSrc(word, pronunciationConfig.type)
+    if (soundUrl === '') return
+
+    const head = document.head
+    const isPrefetch = (Array.from(head.querySelectorAll('link[href]')) as HTMLLinkElement[]).some((el) => el.href === soundUrl)
+
+    if (!isPrefetch) {
+      const audio = new Audio()
+      audio.src = soundUrl
+      audio.preload = 'auto'
+
+      // gpt 说这这两行能尽可能规避下载插件被触发问题。 本地测试不加也可以，考虑到别的插件可能有问题，所以加上保险
+      audio.crossOrigin = 'anonymous'
+      audio.style.display = 'none'
+
+      head.appendChild(audio)
+
+      return () => {
+        head.removeChild(audio)
+      }
+    }
+  }, [pronunciationConfig.type, word])
+}
